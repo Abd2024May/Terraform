ee2f0f2f6cf341cde3f89b67d1a836cfc9ddb07c
Abd2024May <abdelhafez2024may@hotmail.com>
2024-05-19 09:30:03 +0300

    add deployment

--- security_groups.tf
+++ security_groups.tf
@@ -65,6 +65,15 @@ resource "aws_security_group" "k8s_master" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    description      = "Burgan Services"
+    from_port        = 3000
+    to_port          = 3100
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -127,6 +136,15 @@ resource "aws_security_group" "k8s_worker" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    description      = "Burgan Services"
+    from_port        = 3000
+    to_port          = 3100
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  
   egress {
     from_port        = 0
     to_port          = 0
